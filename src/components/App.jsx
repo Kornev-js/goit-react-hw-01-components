b0f88fa5f64c02d7aa65f0d81eb1057f7729ed02
Kornev-js <83174733+Kornev-js@users.noreply.github.com>
2022-11-17 14:07:41 +0300

    rework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import Profile from './Profile/Profile.jsx';
 import Statistics from './Statistics/Statistics.jsx';
-import FriendList from './FriendList/FrienList.jsx';
+import {FriendList} from './FriendList/FriendList.jsx';
 import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
